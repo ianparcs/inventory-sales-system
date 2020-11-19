fd72cd6fbbe1de54370c313edf91559f8132a6ab
Ian Parcon <ianparcs@gmail.com>
2020-11-19 18:38:29 +0800

    removed warnings

---
+++ src/main/java/ph/parcs/rmhometiles/entity/inventory/item/BaseService.java
@@ -0,0 +1,47 @@
+package ph.parcs.rmhometiles.entity.inventory.item;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import ph.parcs.rmhometiles.util.PageUtil;
+
+import java.util.Set;
+
+@Service
+public abstract class BaseService<T extends BaseEntity> implements BaseServiceInterface<T, Integer> {
+
+    protected EntityRepository<T, Integer> entityRepository;
+
+    @Override
+    public Page<T> findPages(int page, int itemPerPage, String name) {
+        PageRequest pageRequest = PageUtil.requestPage(page, itemPerPage);
+        return entityRepository.findAllByNameContains(pageRequest, name);
+    }
+
+    @Override
+    public T findEntityById(Integer id) {
+        return entityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<T> findEntities(String query) {
+        return entityRepository.findAllByNameContains(query);
+    }
+
+    @Override
+    public T saveEntity(T entity) {
+        return entityRepository.save(entity);
+    }
+
+    @Override
+    public boolean deleteEntity(T entity) {
+        entityRepository.delete(entity);
+        return isExist(entity.getId());
+    }
+
+    @Override
+    public boolean isExist(Integer id) {
+        return entityRepository.existsById(id);
+    }
+
+}
